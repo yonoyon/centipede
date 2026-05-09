d300f8dd6ff50542a75e3ea34bd9c71d4984f1f0
yonoyon <yonojouzu@mailbox.org>
2026-05-09 16:01:10 +0200

    renaming, improving structure/logic
    big change. instead of being a general class, this will serve as a class for the sandbox mode from now on. added some methods (moved from main file to improve "cleanliness"). fixed an output issue ("1 moves!")

--- sandbox.py
+++ sandbox.py
@@ -1,18 +1,82 @@
 import sys #sys.exit()
 import random #random.choice()
+import time #time.sleep()
 
-class Centipede:
+class Sandbox:
 
-    def  __init__(self, length): #constructor
+    def  __init__(self, length, commands): #constructor
         self.length = length
+        self.commands = commands
+        self.sandbox_yes = {
+        **commands,
+        "yes" : self.y,
+        "y" : self.y,
+        "yeah" : self.y,
+        "ye" : self.y,
+        "yea" : self.y,
+        "okay" :self.y
+        }
         self.solved_centipede = list(range(1, length + 1))
         self.current_centipede = self.solved_centipede.copy()
         self.scramble()
 
+    def sandbox(self):
+        print("sandbox!")
+        while True: #main loop
+            self.start()
+            self.game()
+            print("Would you like to try again?")
+            user_input = input("Input here: ").strip().lower()
+            if  user_input in self.sandbox_yes:
+                self.sandbox_yes[user_input]()
+                return True
+            else:
+                return False
+    
+    def y(self): 
+        print("Continuing...")
+        time.sleep(1)
+
     def start(self): #start messages
         print(f"You must solve into: {self.solved_centipede}")
         print(f"Your scrambled state is: {self.current_centipede}")
+
+    def game(self): #main game loop
+        x = 0 #move counter
+        while True:
+            move = input("Input move: ").strip().lower()
+            try:
+                i, sign = self.parse_move(move)
+            except ValueError as e:
+                print(f"Invalid Input: {e}")
+                continue
+            if self.permute(i, sign):
+                x+=1
+            else:
+                print("Invalid move. ")
+            if self.check(x):
+                break
+
+    def parse_move(self, move): #only for parsing of moves, not for validation
+        move = move.strip().lower()
+    
+        if move in self.commands:
+            self.commands[move]()
+    
+        if len(move) < 2:
+            raise ValueError("Too short. ")
+
+        try:
+            i = int(move[:-1]) - 1
+        except ValueError: 
+            raise ValueError("Not a valid index. ")
         
+        sign = move[-1]
+        if sign not in "+-":
+            raise ValueError("Not a valid operator. ")
+    
+        return i, sign
+
     def generate(self): #generates possible moves
         moves = []
         for i in range(self.length):
@@ -27,7 +91,6 @@ class Centipede:
             i, sign = random.choice(self.generate())
             self. swap(i, sign)
             #check whether this doesn't scramble it into a solved state already ~ (TBI)
-        
 
     def permute(self, i, sign): #main puzzle mechanic
         if self.validate(i, sign):
@@ -55,6 +118,9 @@ class Centipede:
             self.current_centipede[i], self.current_centipede[i-1]  = -self.current_centipede[i-1], -self.current_centipede[i]
     
     def check(self,x): #if solved, ends program
-        if self.current_centipede == self.solved_centipede:
+        if self.current_centipede == self.solved_centipede and  x > 1:
             print(f"Congratulations! You solved it in {x} moves!")
+            return True
+        if self.current_centipede == self.solved_centipede and x == 1:
+            print(f"Congratulations! You solved it in {x} move!")
             return True
