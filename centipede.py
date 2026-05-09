b295c626f0652e48069c991d83bcce59df65899b
yonoyon <yonojouzu@mailbox.org>
2026-05-09 16:20:42 +0200

    change

--- centipede.py
+++ centipede.py
@@ -1,60 +1,79 @@
+import time #time.sleep()
 import sys #sys.exit()
 import random #random.choice()
 
-class Centipede:
+from sandbox import Sandbox
 
-    def  __init__(self, length): #constructor
-        self.length = length
-        self.solved_centipede = list(range(1, length + 1))
-        self.current_centipede = self.solved_centipede.copy()
-        self.scramble()
+#basic commands (+functions), work globally
 
-    def start(self): #start messages
-        print(f"You must solve into: {self.solved_centipede}")
-        print(f"Your scrambled state is: {self.current_centipede}")
+def help_func():
+    print("help_func!")
+    print("test")
+
+def quit_func():
+    print("Exiting...")
+    time.sleep(1)
+    sys.exit("test")
+
+commands = {
+    "help" : help_func,
+    "h" : help_func,
+    "tutorial" : help_func,
+    "tut" : help_func,
+
+    "quit" : quit_func,
+    "q" : quit_func,
+    "exit" : quit_func,
+    "leave" : quit_func,
+}
+
+
+def levels(): #for levels ~(TBI)
+    print("levels!")
+
+
+def tosandbox(): #initializes object for sandbox mode and starts it
+    while True:
+        try:
+            length = int(input("Input length: ").strip())
+            if length < 3:
+                print("Length can't be this short!")
+            else:
+                centipede_1 = Sandbox(length, commands)
+                if centipede_1.sandbox():
+                    pass
+                else:
+                    break
+        except ValueError:
+            print("Not a valid integer!")
         
-    def generate(self): #generates possible moves
-        moves = []
-        for i in range(self.length):
-            if self.validate(i, "+"):
-                moves.append((i, "+"))
-            if self.validate(i, "-"):
-                moves.append((i, "-"))
-        return moves
+
+"""
+main menu stuff below
+"""
+
+menu_commands = {
+    **commands,
+
+    "levels" : levels,
+    "level" : levels,
+    "l" : levels,
+
+    "sandbox" : tosandbox,
+    "sand" : tosandbox,
+    "s" : tosandbox
+}
+
+def menu(): #main menu
+    print("Welcome! Choose whether you want to play through the levels or enter sandbox mode. ")
+    print("Of course, you can also input help at any time, if needed. If you'd like to quit, input quit. ")
     
-    def scramble(self): #scrambles using generated moves
-        for _ in range(self.length*self.length):
-            i, sign = random.choice(self.generate())
-            self. swap(i, sign)
-            #check whether this doesn't scramble it into a solved state already ~ (TBI)
-        
-
-    def permute(self, i, sign): #main puzzle mechanic
-        if self.validate(i, sign):
-            self.swap(i, sign)
-            print(f"Current state: {self.current_centipede}")
-            return True
-        return False
-
-    def validate(self, i, sign): #validates moves (either generated or input ones)
-        if i < 0 or i >= self.length:
-            #print("Illegal move; position not in bounds. ") ~ possible error message (TBI)
-            return False
-        if sign == "+" and i + 1 >= self.length:
-            #print("Illegal move; right is an impossible direction for given position. ") ~ possible error message (TBI)
-            return False
-        if sign == "-" and i - 1 < 0:
-            #print("Illegal move; left is an impossible direction for given position. ") ~ possible error message (TBI)
-            return False
-        return True
-
-    def swap(self, i, sign): #main puzzle logic. used for scrambling and solving
-        if sign == "+":
-            self.current_centipede[i], self.current_centipede[i+1]  = -self.current_centipede[i+1], -self.current_centipede[i]
+    while True:
+        print("You're in the main menu. ")
+        user_input = input("Input here: ").strip().lower()
+        if user_input in menu_commands:
+            menu_commands[user_input]()
         else:
-            self.current_centipede[i], self.current_centipede[i-1]  = -self.current_centipede[i-1], -self.current_centipede[i]
-    
-    def check(self,x): #if solved, ends program
-        if self.current_centipede == self.solved_centipede:
-            print(f"Congratulations! You solved it in {x} moves!")
-            return True
+            print("Input invalid, try again. ")
+
+menu() #start main menu
