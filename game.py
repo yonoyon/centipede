cebede340ffb1487ebb313eb2d76b1464890a113
yonoyon <yonojouzu@mailbox.org>
2026-05-09 15:58:23 +0200

    separated game modes
    this is the main file. improved menu, logic and structure. game modes are cleanly separated now.

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-import time #sleep()
-import sys #exit()
-import random #choice()
+import time #time.sleep()
+import sys #sys.exit()
+import random #random.choice()
 
-from centipede import Centipede
+from sandbox import Sandbox
 
-#main commands, work globally
+#basic commands (+functions), work globally
 
 def help_func():
     print("help_func!")
@@ -28,85 +28,28 @@ commands = {
 }
 
 
-def parse_move(move): #parses move, extracting position and direction
-    move = move.strip().lower()
-    
-    if move in commands:
-        move[commands]()
-    
-    if len(move) < 2:
-        raise ValueError("Too short. ")
-
-    try:
-        i = int(move[:-1]) - 1
-    except ValueError: 
-        raise ValueError("Not a valid index. ")
-        
-    sign = move[-1]
-    if sign not in "+-":
-        raise ValueError("Not a valid operator. ")
-    
-    return i, sign
-
-def game(centipede_1): #game function/loop
-    x = 0 #move counter
-    while True:
-        move = input("Input move: ")
-        try:
-            i, sign = parse_move(move)
-        except ValueError as e:
-            print(f"Invalid Input: {e}")
-            continue
-        if centipede_1.permute(i, sign):
-            x+=1
-        else:
-            print("Invalid move. ")
-        if centipede_1.check(x):
-           break
-
-def levels(): #for levels
+def levels(): #for levels ~(TBI)
     print("levels!")
 
 
-"""
-dictionary+function, main function for sandbox mode
-"""
-def y(): 
-    print("Continuing...")
-    time.sleep(1)
-
-sandbox_yes = {
-    **commands,
-    "yes" : y,
-    "y" : y,
-    "yeah" : y,
-    "ye" : y,
-    "yea" : y,
-    "okay" :y,
-    }
-
-def sandbox():
-    print("sandbox!")
-    while True: #main loop
+def tosandbox(): #initializes object for sandbox mode and starts it
+    while True:
         try:
             length = int(input("Input length: ").strip())
             if length < 3:
                 print("Length can't be this short!")
-                continue
-            centipede_1 = Centipede(length)
-            centipede_1.start()
-            game(centipede_1)
-            print("Would you like to try again?")
-            user_input = input("Input here: ").strip().lower()
-            if  user_input in sandbox_yes:
-                sandbox_yes[user_input]()
             else:
-                break
+                centipede_1 = Sandbox(length, commands)
+                if centipede_1.sandbox():
+                    pass
+                else:
+                    break
         except ValueError:
             print("Not a valid integer!")
-            
+        
+
 """
-main menu stuff
+main menu stuff below
 """
 
 menu_commands = {
@@ -116,13 +59,12 @@ menu_commands = {
     "level" : levels,
     "l" : levels,
 
-    "sandbox" : sandbox,
-    "sand" : sandbox,
-    "s" : sandbox
-
+    "sandbox" : tosandbox,
+    "sand" : tosandbox,
+    "s" : tosandbox
 }
 
-def menu():
+def menu(): #main menu
     print("Welcome! Choose whether you want to play through the levels or enter sandbox mode. ")
     print("Of course, you can also input help at any time, if needed. If you'd like to quit, input quit. ")
     
@@ -133,6 +75,5 @@ def menu():
             menu_commands[user_input]()
         else:
             print("Input invalid, try again. ")
-    print("passed loop!")
 
-menu()#starts
+menu() #start main menu
