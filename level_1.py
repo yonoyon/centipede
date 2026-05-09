d897c07aabd48bc4d0e02dec10e41d7b332a4e21
yonoyon <yonojouzu@mailbox.org>
2026-05-09 22:47:18 +0200

    added core idea?
    acts as placeholder for now

--- level_1.py
+++ level_1.py
@@ -1,1 +1,36 @@
-# will be the class for level 1
+#just a placeholder for now...
+
+"""
+import time
+
+
+
+class Level_1:
+
+    def __init__(self, commands):
+        self.commands = commands
+
+    def level_1(self):
+        
+            centipede = [1, 3, 2, 5, 6, 4]
+            moves = ["1+", "2+"]
+            print("Welcome to level 1. The goal of any level is to permute the given list into a solved state. ")
+            time.sleep(1)
+            print("A solved state will always look the same: positive numbers from 1 upward. ")
+            time.sleep(1)
+            print(f"Have a try with {centipede}. Input help if you don't know the move notation. ")
+            while True
+                print(centipede)
+                user_input = input("Input: ").strip().lower()
+                if user_input in self.commands:
+                    self.commands[user_input]()
+                    continue
+
+                if user_input != in moves:
+                    print("Invalid")
+
+
+        return True
+
+
+"""
