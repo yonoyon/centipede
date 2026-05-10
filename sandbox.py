3d1bb13958ac0d850246f7b78665c5bedce53c44
yonoyon <yonojouzu@mailbox.org>
2026-05-10 12:01:31 +0200

    added new methods, implemented command case check
    ...

--- sandbox.py
+++ sandbox.py
@@ -1,14 +1,19 @@
+"""
+sandbox is a separate game mode. it is fundamentally different from the levels mode, insofar as user can choose length and move type freely.
+for the sake of simplicity, every aspect of sandbox mode is covered here, from I/O to logic. this is INTENTIONAL.
+one thing that could change regarding this is the methods in general. currently theres no clear separation between what interacts with the user, what actually interacts with the list and what just loops it all...
+"""
+
 import sys #sys.exit()
 import random #random.choice()
 import time #time.sleep()
 
 class Sandbox:
 
-    def  __init__(self, length, commands): #constructor
-        self.length = length
+    def  __init__(self, commands): #constructor
         self.commands = commands
+        self.length = self.get_length()
         self.sandbox_yes = {
-        **commands,
         "yes" : self.y,
         "y" : self.y,
         "yeah" : self.y,
@@ -16,22 +21,45 @@ class Sandbox:
         "yea" : self.y,
         "okay" :self.y
         }
-        self.solved_centipede = list(range(1, length + 1))
+        self.solved_centipede = list(range(1, self.length + 1))
         self.current_centipede = self.solved_centipede.copy()
         self.scramble()
+
+    def get_length(self): #gets length. once different move types get implemented, this could integrate with options() really well.
+        while True:
+            length = input("Input length: ").strip().lower()
+            if length in self.commands:
+                self.commands[length]()
+                continue
+            try:
+                length = int(length)
+                if length < 3:
+                    print("Length can't be this short!")
+                else:
+                    return length
+            except ValueError:
+                print("Not a valid integer!")
 
     def sandbox(self):
         while True: #main loop
             self.start()
             self.game()
-            print("Would you like to try again?")
-            user_input = input("Input here: ").strip().lower()
-            if  user_input in self.sandbox_yes: #add in commands case ~ (TBI)
-                self.sandbox_yes[user_input]()
+            if self.sandbox_end():
                 return True
             else:
                 return False
     
+    def sandbox_end(self):
+        while True:
+            user_input = input("Would you like to try again? Input: ").strip().lower()
+            if  user_input in self.sandbox_yes:
+                self.sandbox_yes[user_input]()
+                return True
+            elif user_input in self.commands:
+                self.commands[user_input]()
+            else:
+                return False
+
     def y(self): 
         print("Continuing...")
         time.sleep(1)
@@ -123,3 +151,13 @@ class Sandbox:
     def check(self): #very simple, "independent" method
         if self.current_centipede == self.solved_centipede:
             return True
+
+""" 
+#something like this will be used once multiple move types are possible
+    def options(self):
+        print("You're in sandbox mode. To proceed, input desired length and move type. ")
+        print("Available move types: adjacent")
+        while True:
+            centipede_length = get_length()
+            return centipede_length
+"""
